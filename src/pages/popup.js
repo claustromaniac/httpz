055b73e4852c9261255f527107322ca4dbd655d3
claustromaniac <20734810+claustromaniac@users.noreply.github.com>
2019-04-26 15:11:18 -0300

    inform users about incognito whitelisting

--- src/pages/popup.js
+++ src/pages/popup.js
@@ -8,9 +8,10 @@ browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
 	if (url.protocol === 'https:') {
 		ui.info.textContent = `${url.hostname} was redirected to HTTPS. If you click the button below, this site will be added to the whitelist and the tab will be reloaded in HTTP.`;
 		ui.whitelist.textContent = 'add to whitelist';
-		ui.whitelist.onclick = e => {
-			ui.whitelist.disabled = true;
-			browser.windows.getLastFocused().then(w => {
+		browser.windows.getLastFocused().then(w => {
+			if (w.incognito) ui.incognito.textContent = 'Note: sites added to the whitelist from a private window will not be visible in the options page';
+			ui.whitelist.onclick = e => {
+				ui.whitelist.disabled = true;
 				browser.runtime.sendMessage({host: url.hostname, value: w.incognito ? 2 : 1}).then(() => {
 					url.protocol = 'http:';
 					browser.tabs.update(
@@ -18,8 +19,8 @@ browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
 						{ loadReplace: true, url: url.toString() }
 					);
 				});
-			});
-		};
+			};
+		});
 	} else {
 		ui.info.textContent = `HTTPZ did not try to redirect ${url.hostname} to HTTPS because it is in the whitelist. If you click the button below, this site will be removed from the whitelist and the tab will be reloaded in HTTPS.`;
 		ui.whitelist.textContent = 'remove from whitelist';
