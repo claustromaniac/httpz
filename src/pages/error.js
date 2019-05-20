0caea1657030ffc3b8a129efc1fa6266ab66fb87
claustromaniac <20734810+claustromaniac@users.noreply.github.com>
2019-05-20 09:31:59 -0300

    error.htm retry button
    Closes #21

--- src/pages/error.js
+++ src/pages/error.js
@@ -7,10 +7,16 @@ browser.tabs.getCurrent().then(tab => {
 	const target = new URL(url.searchParams.get('target'));
 	ui.hostname.textContent = target.hostname;
 	ui.continue.onclick = e => {
+		ui.retry.disabled = true;
 		ui.continue.disabled = true;
 		browser.runtime.sendMessage({ignore: target.hostname}).then(() => {
 			target.protocol = 'http:';
 			browser.tabs.update(tab.id, {loadReplace: true, url: target.toString()});
 		});
+	};
+	ui.retry.onclick = e => {
+		ui.retry.disabled = true;
+		ui.continue.disabled = true;
+		browser.tabs.update(tab.id, {loadReplace: true, url: target.toString()});
 	};
 });
