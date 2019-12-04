8ce921f22ad0cbe22ddc46b51893d07b6b55a1f2
claustromaniac <20734810+claustromaniac@users.noreply.github.com>
2019-12-04 14:17:57 -0300

    update popup

--- src/pages/popup.js
+++ src/pages/popup.js
@@ -6,10 +6,13 @@ browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
 	const tab = tabs[0];
 	const url = new URL(tab.url);
 	if (url.protocol === 'https:') {
-		ui.info.textContent = `${url.hostname} was redirected to HTTPS. If you click the button below, HTTPZ will add this site to the list of exclusions and will attempt to reload it over HTTP`;
+		ui.info.textContent = `HTTPZ redirected http://${url.hostname}/ to HTTPS.\n\nIf the page does not seem to work well over HTTPS, you can attempt to reload it over HTTP and exclude the site from future redirections.`;
 		ui.whitelist.textContent = 'Add to exclusions';
 		const incognito = browser.extension.inIncognitoContext;
-		if (incognito) ui.incognito.textContent = 'Note: sites excluded from a private window will not be visible in the options page';
+		if (incognito) {
+			ui.incognito.textContent = 'Note: sites excluded from a Private Browsing window will not be visible in the options page';
+			ui.incognito.style.display = 'block';
+		}
 		ui.whitelist.onclick = e => {
 			ui.whitelist.disabled = true;
 			url.protocol = 'http:';
@@ -21,7 +24,7 @@ browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
 			}).then(() => {close();});
 		};
 	} else {
-		ui.info.textContent = `HTTPZ did not try to redirect ${url.hostname} to HTTPS because it is in the list of exclusions. If you click the button below, this site will be removed from that list and the tab will be reloaded`;
+		ui.info.textContent = `HTTPZ did not try to redirect http://${url.hostname}/ to HTTPS because it is in the list of exclusions.\n\nIf you click the button below, this site will be removed from that list and the tab will be reloaded`;
 		ui.whitelist.textContent = 'Remove from exclusions';
 		ui.whitelist.onclick = e => {
 			ui.whitelist.disabled = true;
